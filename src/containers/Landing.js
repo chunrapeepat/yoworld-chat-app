4046887e6d6caba6ecf3e806ba7726e8d77f4b67
chunza2542 <chunza2542@hotmail.com>
2018-07-15 11:47:18 +0700

    add picture to home screen

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Ink from 'react-ink'
 
 import {Center, FixedContainer} from '../core/styled'
-import {fonts, fontSize} from '../core/helper'
+import {fonts, fontSize, speak} from '../core/helper'
 
 const LoginWithFacebook = styled.button`
   position: relative;
@@ -20,13 +20,28 @@ const LoginWithFacebook = styled.button`
   font-size: ${fontSize.header}px;
 `
 
-export default ({login}) => (
-  <FixedContainer>
-    <Center>
-      <LoginWithFacebook onClick={login}>
-        <Ink />
-        Login With Your Facebook Account
-      </LoginWithFacebook>
-    </Center>
-  </FixedContainer>
-)
+const Profile = styled.img`
+  width: 1000px;
+  margin-bottom: 20px;
+  cursor: pointer;
+`
+
+export default class extends Component {
+  sayHello() {
+    speak('Hey yo. Welcome', 2)
+  }
+
+  render() {
+    return (
+      <FixedContainer>
+        <Center>
+          <center><Profile onClick={this.sayHello} src="https://cbsnews1.cbsistatic.com/hub/i/r/2018/04/26/3a2033a4-9384-4d20-b63f-a96249c51a35/thumbnail/620x350/21dd96b19bdacb9ef83f03e9f45bbe67/gettyimages-529940582.jpg"/>
+          <LoginWithFacebook onClick={this.props.login}>
+            <Ink />
+            Login With Your Facebook Account
+          </LoginWithFacebook></center>
+        </Center>
+      </FixedContainer>
+    )
+  }
+}
